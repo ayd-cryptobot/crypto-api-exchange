cbb861f628ff6bce962e543be29a5c142e879f9d
sagarus2020 <juanpablocq@ufps.edu.co>
2022-11-26 09:45:37 -0500

    follow updated

--- exhange.sql
+++ exhange.sql
@@ -15,7 +15,7 @@ CREATE TABLE `follow` (
   KEY `fk_crypto_name_idx` (`crypto_name`),
   CONSTRAINT `fk_crypto_name` FOREIGN KEY (`crypto_name`) REFERENCES `crypto` (`crypto_name`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 CREATE TABLE `user` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `telegram_id` int NOT NULL,
@@ -23,4 +23,4 @@ CREATE TABLE `user` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `telegram_id_UNIQUE` (`telegram_id`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- src/servicios/exchange.js
+++ src/servicios/exchange.js
@@ -31,7 +31,7 @@ const nodeCron = require("node-cron");
 //exchange variables
 var crypto_name;
 
-var currency_pair;
+var currency_pair = "";
 var query_schedule;
 var result;
 
@@ -46,10 +46,12 @@ async function geckoPing() {
 
 };
 
-async function createFollow(user_id, crypto_name) {
+async function createFollow(user_id, crypto_name,currency_pair, query_schedule) {
   let i = 0;
   while (i < crypto_name.length) {
-    var sql = "INSERT INTO follow (user_id,crypto_name) VALUES ('" + user_id + "','" + crypto_name[i] + "');";
+    var sql = "UPDATE user SET query_schedule ='"+query_schedule+"' WHERE user_id="+ user_id;
+    result = await con.query(sql)
+    var sql = "INSERT INTO follow (user_id,crypto_name,currency_pair) VALUES ('" + user_id + "','" + crypto_name[i] + "','"+currency_pair+"');";
     result = await con.query(sql)
     console.log("1 follow inserted");
 
@@ -127,10 +129,9 @@ endpoints.post('/exchange/crypto/follow', async (req, res) => {
 async function notify(user_id) {
   try {
     var user_id = user_id;
-    var currency_pair
+
     var array_crypto = [];
     var sql;
-    var array_coins = [];
 
     var rta = [];
     //conn.execute(async function(err) {
@@ -140,21 +141,17 @@ async function notify(user_id) {
     console.log("Connected!");
 
     sql = "SELECT crypto_name,currency_pair FROM follow WHERE  (user_id='" + user_id + "')";
-    result = await con.query(sql)
-    console.log(JSON.stringify(result) + "query check");
-    currency_pair = await result[0].currency_pair;
+    var result = await con.query(sql)
+    result = await result[0]
+    currency_pair = "" + result[0].currency_pair;
+
     for (crypto_rta of result) {
       await array_crypto.push(crypto_rta.crypto_name);
-      console.log(array_crypto + "check")
 
     }
-    cryptos = await array_crypto;
 
-    for (crypto_rta of cryptos) {
-      array_coins.push(crypto_rta.crypto_name);
-    }
-    if (cryptos && cryptos.length > 0) {
-      for (let crypto of cryptos) {
+    if (array_crypto && array_crypto.length > 0) {
+      for (let crypto of array_crypto) {
 
         const response = await axios.get(config.api_exchange_history + crypto + '/market_chart?vs_currency=' + currency_pair + '&days=' + 0 + '&interval=daily')
 
@@ -177,58 +174,66 @@ async function notify(user_id) {
     }
   } catch (error) {
     console.log(error)
-    res.json("error on notification")
-    res.end
+
   }
-  return rta
+  if (rta) return rta
+  else return
+
 }
 
-async function usersQuery(query_schedule) {
-
+async function usersQuery(sql) {
+  console.log(sql + "outworks")
   var users_array = []
-  await nodeCron.schedule(query_schedule, async () => {
-    console.log(query_schedule + "works")
-    result = await con.query(sql)
-    await console.log(JSON.stringify(result));
-    for (users of result) {
-     await  users_array.push(
-        message = {
-          user: users.telegram_id,
-          coin: await notify(users.user_id)
-        })
-      // await delay();
+  // await nodeCron.schedule(query_schedule, async () => {
+  console.log(sql + "works")
+  result = await con.query(sql)
+if(result[0]){
+  for (users of result[0]) {
+    var notification = await notify(users.user_id)
+    message = {
+      user: users.telegram_id,
+      coin: notification
     }
+    if (message) {
+      await users_array.push(message)
+    }
+  }
+}
 
 
-  })
+  console.log(users_array)
+
+  // })
   return users_array
 }
 
 async function schedule() {
   var users_array = []
-  query_schedule = "*/30 * * * *"
+  array_query = ["*/30 * * * *", "* */1 * * *", "* */2 * * *"]
+  for (query_schedule of array_query) {
+    try {
+      sql = "SELECT user_id, telegram_id FROM user WHERE query_schedule='" + query_schedule + "';"
+      var array = await usersQuery(sql)
+      if (array&&array!==null&&array!=="null"&&array.length>0){
+      await users_array.push(array[0])
+    }
+    }
+    catch(error) {console.log(error) }
+  }
 
-  sql = "SELECT user_id, telegram_id FROM user WHERE (query_schedule='" + query_schedule + "')"
-  await users_array.push(await usersQuery(query_schedule))
-
-  query_schedule = "* */1 * * *"
-  sql = "SELECT user_id, telegram_id FROM user WHERE (query_schedule='" + query_schedule + "')"
-  await users_array.push(await usersQuery(query_schedule))
-
-  query_schedule = "* */2 * * *"
-  sql = "SELECT user_id, telegram_id FROM user WHERE (query_schedule='" + query_schedule + "')"
-  await users_array.push(await usersQuery(query_schedule))
-
+  
   return await users_array;
 }
 
 endpoints.get('/exchange/crypto/notify/', async (req, res) => {
   await PromiseConnection();
   try {
+
     res.json(await schedule())
     res.end
   }
   catch (err) {
+    console.log(err)
     res.json("internal fuction error")
     res.end
   }
@@ -241,64 +246,67 @@ endpoints.post('/exchange/crypto/price', async (req, res) => {
   console.log(req.body, "este es el body")
   try {
     await PromiseConnection();
-    var actual_date=new Date()
-    var date=new Date()
+    var actual_date = new Date()
+    var date = new Date()
 
-    var crypto =req.body.crypto
-    var range =req.body.dateRange
+    var crypto = req.body.crypto
+    var range = req.body.dateRange
     var arregloCryptos = []
-     actual_date;
-     console.log (date)
+    actual_date;
+    console.log(date)
     const response = await axios.get(config.api_exchange_history + crypto + '/market_chart?vs_currency=USD&days=' + range + '&interval=daily')
     const datos = response.data.prices
     let cryptoMoneda = {
     }
-    delay()
+
     const valores = datos
     const historic_price = []
-    actual_date.setDate(actual_date.getDate()-valores.length+1)
+    actual_date.setDate(actual_date.getDate() - valores.length + 1)
     for (let valor of valores) {
-      date =date_format.format(actual_date,'DD/MM/YYYY')
+      date = date_format.format(actual_date, 'DD/MM/YYYY')
       const valorGuardar = await parseFloat(valor[1].toFixed(3))
       historic_price.push({
-       "date" : date,
-       "price": valorGuardar
+        "date": date,
+        "price": valorGuardar
       }
-        )
+      )
 
-      delay()
-      actual_date.setDate(actual_date.getDate()+1)
+
+      actual_date.setDate(actual_date.getDate() + 1)
     }
-    cryptoMoneda= historic_price
+    cryptoMoneda = historic_price
     console.log(cryptoMoneda)
-    arregloCryptos=cryptoMoneda
+    arregloCryptos = cryptoMoneda
 
 
   } catch (error) {
     // console.log(error)
   }
-  var message={
+  var message = {
     "name": crypto,
     "currency_pair": "USD",
-    "historic_price":arregloCryptos
+    "historic_price": arregloCryptos
 
   }
-console.log(message)
+  console.log(message)
 
   res.json(message);
   res.end;
 })
 
-endpoints.post('/news/accounts/event', async (req, res) => {
-  try{
-    console.log(req.body, "este es el body")
+endpoints.post('/exchange/accounts/event', async (req, res) => {
+  try {
+
+    const buff = Buffer.from(req.body.message.data, 'base64');
+    const id = JSON.parse(buff.toString('utf-8'))
+    console.log(id, "este es el body")
     await PromiseConnection();
-  
+
     //const buff = Buffer.from(req.body.message.data, 'base64');
     //const buff = Buffer.from(req.body.message.data, 'base64');
-    const buff = req.body.message.data;
+
     //const id=buff.toString('utf-8')
-    const id = buff;
+
     try {
       var operation_type = id.operation_type
       console.log(operation_type);
@@ -310,7 +318,7 @@ endpoints.post('/news/accounts/event', async (req, res) => {
     console.log("Connected!");
     var telegram_user_id = id.telegram_user_id
     switch (operation_type) {
-  
+
       case ("create"):
         // var first_name=id.first_name
         // var last_name=id.last_name
@@ -320,40 +328,64 @@ endpoints.post('/news/accounts/event', async (req, res) => {
         var sql = "INSERT INTO user (telegram_id) VALUES ('" + telegram_user_id + "');";
         result = await con.query(sql)
         await res.json({ "message": "account created" });
+
         break;
-  
-      case ("edit"):
-        // var first_name=id.first_name
-        // var last_name=id.last_name
-        // var email=id.email
-        // var username=id.username
-        //     var sql = "UPDATE user SET first_name='"+first_name+"',last_name='"+ last_name+"',email='"+ email+"',username='"+ username+"' WHERE (telegram_id='"+telegram__user_id+"');";
-        var query_schedule = id.query_schedule;
-  
-        var sql = "UPDATE user SET query_schedule='" + query_schedule + "' WHERE (telegram_id='" + telegram_user_id + "');";
-        result = await con.query(sql)
-  
-        await res.json({ "message": "account edited" });
+
+      case ("update"):
+
+
+        await res.json({ "message": "ok" });
         break;
-  
+
       case ("delete"):
         var sql = "DELETE FROM user WHERE (telegram_id='" + telegram_user_id + "');";
         result = await con.query(sql)
         await res.json({ "message": "account deleted" });
+
         break;
-  
+
       default:
         await res.json({ "error": "event not found" });
+
         break;
     }
-  
+
     res.end
   }
-  catch(err){
+  catch (err) {
     console.log(err)
-    res.json("error")
+    await res.json("error")
+
+
     res.end
   }
-  })
-  
+})
+
+//gcloud auth application-default login   
+/**
+ * TODO(developer): Uncomment these variables before running the sample.
+ */
+// const topicNameOrId = 'YOUR_TOPIC_NAME_OR_ID';
+// const data = JSON.stringify({foo: 'bar'});
+
+// Imports the Google Cloud client library
+const { PubSub } = require('@google-cloud/pubsub');
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub();
+//GOOGLE_APPLICATION_CREDENTIALS = '.\cryptobot-369523'
+async function publishMessage(messaging) {
+  // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+  const dataBuffer = Buffer.from(messaging);
+
+  try {
+    const messageId = await pubSubClient
+      .topic("projects/cryptobot-369523/topics/crypto-prices-topic")
+      .publishMessage({ data: dataBuffer });
+    console.log(`Message ${messageId} published.`);
+  } catch (error) {
+    console.error(`Received error while publishing: ${error.message}`);
+    process.exitCode = 1;
+  }
+}
 module.exports = endpoints
